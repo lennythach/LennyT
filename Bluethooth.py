46c2b9dae14799f2f6d052d3c6eaf8ea8d385f00
Lenny Thach <lennythach1@gmail.com>
2020-06-05 10:45:07 -0700

    bluetoothUpdate

--- Bluethooth.py
+++ Bluethooth.py
@@ -7,13 +7,12 @@ from bluetooth.ble import BeaconService #<--beacon service is a third party modu
 #obstantiating the object
 service = BeaconService() #<--Creating an instance object of the class library
 
-service.start_advertising("1111111111-2222-3333-4444-5555555555555",1,1,1, 200)#<--adding a uuid < advertise the uuid and different ports for spoofing device
+service.start_advertising("11111111-2222-3333-4444-5555555555555",1,1,1, 200)#<--adding a uuid < advertise the uuid and different ports for spoofing device
 #everytime 15 sec you want to spoof a device
 time.sleep(15)
 service.stop_advertise()
 
 print("Done.")
-
 
 
 
